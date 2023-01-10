f72d7428406f159642f13a285b9b9bbcb64ee0cf
Alex <albredmy@gmail.com>
2023-01-10 21:09:31 +0300

    Задание 2

---
+++ src/Bus.java
@@ -0,0 +1,7 @@
+public class Bus extends Transport{
+
+    public Bus(String brand, String model, String color, int maxSpeed) {
+        super(brand, model, color, maxSpeed);
+    }
+
+}
--- src/Main.java
+++ src/Main.java
@@ -5,5 +5,14 @@ public class Main {
 
         System.out.println(ford);
 
+        Bus gasel = new Bus("Gasel", "2330", "blue", 150);
+        Bus mers = new Bus("Mersedes", "A1", "green", 200);
+        Bus ikarus = new Bus("Ikarus", "b133", "brown", 180);
+
+        System.out.println(gasel );
+        System.out.println(mers );
+        System.out.println(ikarus );
+
+
     }
 }
